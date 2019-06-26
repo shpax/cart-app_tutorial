009837420385d68d1d6c83fac95e8a3777255a9e
Maxim Piven <shpax1@gmail.com>
2019-06-26 16:00:14 +0300

    added check tasks

---
+++ tasks/t2.js
@@ -0,0 +1,15 @@
+(async function () {
+  console.log('script start');
+
+  setTimeout(function () {
+    console.log('setTimeout');
+  }, 0);
+
+  await Promise.resolve().then(function () {
+    console.log('promise1');
+  }).then(function () {
+    console.log('promise2');
+  });
+
+  console.log('script end');
+})();
